1e9e94f9513b3791742e3a2aa73652a08c7f74d8
Jackson <jackson97042@gmail.com>
2026-03-29 21:22:43 +0800

    feat: hw2 q1

--- hw2/src/HW2.java
+++ hw2/src/HW2.java
@@ -69,23 +69,67 @@ class Row { // represent a row of fruits
 
 	// returns a new row by adding fruit to the end of the row
 	Row extendedWith(int fruit) {
-
-    throw new Error("method extendedWith(int fruit) to be completed (Question 1)");
+    int[] extended_fruits = new int[this.fruits.length + 1];
+    System.arraycopy(this.fruits, 0, extended_fruits, 0, this.fruits.length);
+    extended_fruits[extended_fruits.length -1] = fruit;
+		return new Row(extended_fruits);
 	}
 
 	// return the list of all stable rows of width width
 	static LinkedList<Row> allStableRows(int width) {
 
 
-    throw new Error("method allStableRows(int width) to be completed (Question 1)");
+    LinkedList<Row> rows = new LinkedList<>();
+
+
+
+    if (width <= 0) {
+      rows.add(new Row());
+      return rows;
+    }
+
+    rows.add(new Row(new int[]{0}));
+    rows.add(new Row(new int[]{1}));
+
+    while (rows.getLast().fruits.length < width) {
+      var tail = rows.getLast();
+      if (tail.fruits.length == 1) {
+        rows.addFirst(tail.extendedWith(0));
+        rows.addFirst(tail.extendedWith(1));
+        rows.removeLast();
+      } else {
+        if (tail.fruits[tail.fruits.length -1] != 0 || tail.fruits[tail.fruits.length -2] != 0 ) {
+          rows.addFirst(tail.extendedWith(0));
+        } 
+        
+        if (tail.fruits[tail.fruits.length -1] != 1 || tail.fruits[tail.fruits.length -2] != 1 ) {
+          rows.addFirst(tail.extendedWith(1));
+        }
+
+        rows.removeLast();
+      }
+    }
+    
+    return rows;
 	}
 
 
 	// check if the row can be stacked with rows r1 and r2
 	// without having three fruits of the same type adjacent
 	boolean areStackable(Row r1, Row r2) {
+    if (
+      r1.fruits.length != r2.fruits.length || 
+      r1.fruits.length != this.fruits.length || 
+      r2.fruits.length != this.fruits.length  
+    ) {
+      return false;
+    }
+    
+		for (int i = 0; i < this.fruits.length; i++) {
+      if (this.fruits[i] == r1.fruits[i] && this.fruits[i] == r2.fruits[i]) return false;
+    }
 
-    throw new Error("method areStackable(Row r1, Row r2) to be completed (Question 1)");
+    return true;
 	}
 }
 
