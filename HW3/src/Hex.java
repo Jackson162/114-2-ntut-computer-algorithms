1c928f8d9cb9a947a712bb17718bde41d29da5ce
Jackson <jackson97042@gmail.com>
2026-04-12 17:50:59 +0800

    feat: hw3 1.3 done

--- HW3/src/Hex.java
+++ HW3/src/Hex.java
@@ -108,6 +108,36 @@ public class Hex {
 
   }
 
+  boolean red_wins(Vector<Integer> uppers, Vector<Integer> lowers) {
+
+    for (var k = 0; k < uppers.size(); k++) {
+      var upper_id = this.find(uppers.get(k));
+      for (var l = 0; l < lowers.size(); l++) {
+        var lower_id = this.find(lowers.get(l));
+
+        if (upper_id == lower_id) return true;
+        
+      }
+    }
+
+    return false;
+  }
+
+    boolean blue_wins(Vector<Integer> left, Vector<Integer> right) {
+
+    for (var k = 0; k < left.size(); k++) {
+      var left_id = this.find(left.get(k));
+      for (var l = 0; l < right.size(); l++) {
+        var right_id = this.find(right.get(l));
+
+        if (left_id == right_id) return true;
+        
+      }
+    }
+
+    return false;
+  }
+
   // return the winning player, or Player.NOONE if no player has won yet
   Player winner() {
     var links_size = (this.playable_n+2) * (this.playable_n+2);
@@ -126,6 +156,26 @@ public class Hex {
         var cur = this.grid[j][i];
         var cur_id = this.getID(i, j);
         if (cur == Player.NOONE) continue;
+        // check top left grid
+        if (j-1 >= 0) {
+          if (cur == this.grid[j-1][i]) adj_links.add(new adj_link(cur_id, this.getID(i, j-1)));
+        }
+
+        // check left grid
+        if (i-1 >= 0) {
+          if (cur == this.grid[j][i-1]) adj_links.add(new adj_link(cur_id, this.getID(i-1, j)));
+        }
+
+        // check down left grid
+        if (i-1 >= 0 && j+1 < this.playable_n+2) {
+          if (cur == this.grid[j+1][i-1]) adj_links.add(new adj_link(cur_id, this.getID(i-1, j+1)));
+        }
+
+        // check up right grid
+        if (j-1 >= 0 && i+1 < this.playable_n+2) {
+          if (cur == this.grid[j-1][i+1]) adj_links.add(new adj_link(cur_id, this.getID(i+1, j-1)));
+        }
+
         // check right grid
         if (i+1 < this.playable_n+2) {
           if (cur == this.grid[j][i+1]) adj_links.add(new adj_link(cur_id, this.getID(i+1, j)));
@@ -138,12 +188,38 @@ public class Hex {
       }
     }
 
+    // red
+    var uppers = new Vector<Integer>();
+    var lowers = new Vector<Integer>();
+
+    for (var i = 1; i < this.playable_n+1; i++) {
+      uppers.add(this.getID(i, 0));
+      lowers.add(this.getID(i, this.playable_n+1));
+    }
+
+    // blue
+    var left = new Vector<Integer>();
+    var right = new Vector<Integer>();
+
+    for (var j = 1; j < this.playable_n+1; j++) {
+      left.add(this.getID(0, j));
+      right.add(this.getID(this.playable_n+1, j));
+    }
+
     // skip randomization: check if there is a path after every union will get conclusion faster
 
     for (var k =0; k < adj_links.size(); k++) {
       var idi = adj_links.get(k).idi();
       var idj = adj_links.get(k).idj();
       union(idi, idj);
+
+      if (this.red_wins(uppers, lowers)) {
+        this.cur_player =  Player.NOONE;
+        return Player.RED;
+      } else if (this.blue_wins(left, right)) {
+        this.cur_player =  Player.NOONE;
+        return Player.BLUE;      
+      }
     }
 
 
